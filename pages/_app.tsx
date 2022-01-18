f1cfcd470a9c82f5a7c2d0f9eac8e5c16ce468ff
Nikita Ershov <nicknamme@MacBook-Air-Nikita.local>
2022-01-18 21:14:14 +0300

    Added Htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps } :AppProps) : JSX.Element  {
+  return (<>
+	<Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+		<link rel="preconnect" href="https://fonts.googleapis.com"/>
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+    </Head>
+	<Component {...pageProps} />
+  </>);
 }
 
-export default MyApp
+export default MyApp;
