def2b2333bb753fa39b0628931cfa58db0c35378
Nikita Ershov <nicknamme@MacBook-Air-Nikita.local>
2022-01-19 10:52:47 +0300

    Added Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag, P } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 
 export default function Home(): JSX.Element {
@@ -9,7 +9,9 @@ export default function Home(): JSX.Element {
 		<Button appearance='primary'>button</Button>
 		<Button appearance='ghost' arrow='right'>button</Button>
 		<P size='s'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, eum! Eum enim laudantium fugit porro asperiores natus reiciendis tempore, laboriosam dolores itaque est alias error facere quam, sed vel vero.</P>
-		
+		<Tag size='s' color='green'>small</Tag>
+		<Tag size='m' color='red'>red</Tag>
+		<Tag  color='gray'>gray</Tag>
 	</>
   );
 }
