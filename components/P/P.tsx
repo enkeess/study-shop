44d007b72b6553772de051f7241bdb762c81090f
Nikita Ershov <nicknamme@MacBook-Air-Nikita.local>
2022-01-19 10:03:28 +0300

    Added P component

---
+++ components/P/P.tsx
@@ -0,0 +1,11 @@
+import { PProps } from './P.props';
+import styles from './P.module.css';
+import cn from 'classnames';
+
+export const P = ({size = 'm', children}:PProps):JSX.Element => {
+	return <p className={cn(styles.p, {
+		[styles.s]: size == 's',
+		[styles.m]: size == 'm',
+		[styles.l]: size == 'l',
+	})}> {children} </p>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 
 export default function Home(): JSX.Element {
   return (
     <>
 		<Htag tag='h1'> Hello </Htag>
+		<Button appearance='primary'>button</Button>
+		<Button appearance='ghost' arrow='right'>button</Button>
+		<P size='s'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, eum! Eum enim laudantium fugit porro asperiores natus reiciendis tempore, laboriosam dolores itaque est alias error facere quam, sed vel vero.</P>
+		
 	</>
   );
 }
